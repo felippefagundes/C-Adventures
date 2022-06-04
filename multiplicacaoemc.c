c3e26bfa8c46e3a40b0e88d828696f35e599f95c
felnin <felippefjleite@gmail.com>
2022-06-04 01:44:23 -0300

    multiplicacaoComC

---
+++ multiplicacaoemc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int x, y;
+	printf("Digite o primeiro numero: \n");
+	scanf("%d", &x);
+	printf("Digite o segundo numero: \n");
+	scanf("%d", &y);
+
+	
+
+	printf("O resultado da multiplicação é: %d", x*y);
+		
+
+}
